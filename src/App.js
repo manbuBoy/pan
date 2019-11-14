54bcfc0ff19491688e4db2d917cfab81d93ca29a
manbuBoy <1365633251@qq.com>
2019-11-14 23:40:26 +0800

    '222'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './com/home'
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Home />
+      </div>
     </div>
   );
 }
